37429d0bf10a41130b9c31ef998b4419481f9192
Nishanth2206 <nishanthskvd@gmail.com>
2025-08-23 15:41:04 +0530

    Add .gitignore

--- springapp/src/main/java/com/examly/springapp/repository/PriorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.examly.springapp.model.Priority;
-
-public interface PriorityRepository extends JpaRepository<Priority, Long>{
-
-}
